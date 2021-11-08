f32a08e07774b064e13018f19ba820bba026cfa8
Simon Laux <mobile.info@simonlaux.de>
2021-11-08 18:02:35 +0100

    add benchmarks

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,41 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use dc_message_parser::parser::{parse_desktop_set, parse_markdown_text, parse_only_text};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let testdata = include_str!("testdata.md");
+    let lorem_ipsum_txt = include_str!("lorem_ipsum.txt");
+    let r10s_update_message = include_str!("r10s_update_message.txt");
+
+    c.bench_function("only_text_lorem_ipsum.txt", |b| {
+        b.iter(|| parse_only_text(black_box(lorem_ipsum_txt)))
+    });
+    c.bench_function("desktop_set_lorem_ipsum.txt", |b| {
+        b.iter(|| parse_desktop_set(black_box(lorem_ipsum_txt)))
+    });
+    c.bench_function("markdown_lorem_ipsum.txt", |b| {
+        b.iter(|| parse_markdown_text(black_box(lorem_ipsum_txt)))
+    });
+
+    c.bench_function("only_text_testdata.md", |b| {
+        b.iter(|| parse_only_text(black_box(testdata)))
+    });
+    c.bench_function("desktop_set_testdata.md", |b| {
+        b.iter(|| parse_desktop_set(black_box(testdata)))
+    });
+    c.bench_function("markdown_testdata.md", |b| {
+        b.iter(|| parse_markdown_text(black_box(testdata)))
+    });
+
+    c.bench_function("only_text_r10s_update_message.txt", |b| {
+        b.iter(|| parse_only_text(black_box(r10s_update_message)))
+    });
+    c.bench_function("desktop_set_r10s_update_message.txt", |b| {
+        b.iter(|| parse_desktop_set(black_box(r10s_update_message)))
+    });
+    c.bench_function("markdown_r10s_update_message.txt", |b| {
+        b.iter(|| parse_markdown_text(black_box(r10s_update_message)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ benches/testdata.md
@@ -0,0 +1,15 @@
+
+HELLO world this is a test document for the benchmarking process, it contains **bold**, *italics* and ~~struck through sections~~.
+
+
+
+email@host email@domain.tld mailto:email@exxample.com
+
+https://delta.chat?test=1234&y=4
+http://delta.chat?test=1234&y=4
+
+https://delta.chat/hello?test=1234&y=4
+
+Then a text containing a delimited link <https://delta.chat/hello?test=1234&y=4> then a [labeled link](https://delta.chat/hello?test=1234&y=4) and a #hashtag, cause why not.
+
+`inline code` and more useless text: 1+1 != 3 ; what a user may or may not write in a message somehow.
