bb96a3ae04af32bb80b87bb1754eb8914ae04b9d
Simon Laux <mobile.info@simonlaux.de>
2023-09-28 19:24:41 +0200

    fix wasm smoke test

--- message_parser_wasm/tests/web.rs
+++ message_parser_wasm/tests/web.rs
@@ -3,19 +3,12 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
-
 use message_parser_wasm::*;
-
 use serde_json::json;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
-
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
 
 use wasm_bindgen::JsValue;
 
@@ -28,15 +21,7 @@ extern "C" {
 #[wasm_bindgen_test]
 fn test_parse() {
     assert_eq!(
-        parse("**Block**"),
-        JsValue::from_serde(&json!([
-            {"t":"Bold","c":[{"t":"Text","c":"Block"}]}
-        ]))
-        .unwrap()
-    );
-
-    assert_eq!(
-        stringify(&parse("**`Block`**")),
-        JsValue::from_str(r#"{"t":"Bold","c":[{"t":"InlineCode","c":{"content":"Block"}}]}"#)
+        stringify(&parse_text("**`Block`**", true)),
+        JsValue::from_str(r#"[{"t":"Bold","c":[{"t":"InlineCode","c":{"content":"Block"}}]}]"#)
     ); // this test needs somekind of deep equal because the order of the properties is not fixed
 }
