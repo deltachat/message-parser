dc5b938bc44d28c9545d44b90e06882dfd4adab5
Farooq Karimi Zadeh <fkz@riseup.net>
2024-05-28 11:49:49 +0330

    add benchmarking for get_first_emoji

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use deltachat_message_parser::parser::{
-    parse_desktop_set, parse_markdown_text, parse_only_text, LinkDestination,
+    parse_desktop_set,
+    parse_markdown_text,
+    parse_only_text,
+    LinkDestination,
+    is_emoji::get_first_emoji,
 };
 
 pub fn criterion_benchmark(c: &mut Criterion) {
@@ -8,6 +12,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let lorem_ipsum_txt = include_str!("lorem_ipsum.txt");
     let r10s_update_message = include_str!("r10s_update_message.txt");
     let links = include_str!("moar_links.txt");
+    let emojies = vec![
+        "not an emoji",
+        "\u{3299}\u{3300}",
+        "🟠",
+        "",
+        "🟠y",
+        "🍏",
+        "lorem",
+        "😀",
+    ];
 
     c.bench_function("only_text_lorem_ipsum.txt", |b| {
         b.iter(|| parse_only_text(black_box(lorem_ipsum_txt)))
@@ -41,6 +55,9 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("parse_link_moar_links.txt", |b| {
         b.iter(|| LinkDestination::parse(black_box(links)))
+    });
+    c.bench_function("emoji", |b| {
+        b.iter(|| emojies.iter().map(|s| get_first_emoji(black_box(s))))
     });
 }
 
